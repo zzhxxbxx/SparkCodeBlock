0663bedc77c3fd311c9560b3a96540ccf80da55d
zhangzhihao <zzh@fjreading.com>
2019-02-19 11:35:52 +0800

    创建第一个scala文件

---
+++ SparkOnScala/src/main/scala/createContext/CreateSparkContext.scala
@@ -0,0 +1,13 @@
+package createContext
+
+/**
+  * @Auther: Administrator
+  * @Date: 2019/2/19 11:28
+  * @Description:
+  */
+object CreateSparkContext {
+	def main(args: Array[String]): Unit = {
+
+	}
+
+}
